4d0f458112c9b466345288e1b5b5b3a6fc1e9708
770menahem <mnjol770@gmail.com>
2021-07-14 13:02:49 +0300

    fix: move py to //

--- src/routes/main.route.ts
+++ src/routes/main.route.ts
@@ -2,12 +2,12 @@ import express = require('express');
 import splitterRoute from './splitter.route';
 import recoveryPostRoute from './recovery.post.route';
 import recoveryGetRoute from './recovery.get.route';
-import pyRoute from './py.route';
+// import pyRoute from './py.route';
 export const router = express.Router();
 
 router.use('/splitter', splitterRoute);
 router.use('/recovery', recoveryPostRoute);
 router.use('/get', recoveryGetRoute);
-router.use('/py', pyRoute);
+// router.use('/py', pyRoute);
 
 export default router;
---
+++ src/service/py.service.ts
@@ -0,0 +1,6 @@
+// export default {
+//   source: async (source: string) => await source,
+//   fromSource: async (source: string, identifier: string) =>
+//     (await source) + identifier,
+//   byIdentifier: async (identifier: string) => await identifier,
+// };
