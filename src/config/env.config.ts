28057a8080afc03c090fb6da940e855fb1fa99fa
770menahem <mnjol770@gmail.com>
2021-08-10 10:34:48 +0300

    update retry

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -21,11 +21,8 @@ export default {
     uri: env.get('RABBIT_URI').required().asString(),
     logger: env.get('LOGGER').required().asString(),
     retryOptions: {
-      minTimeout: env
-        .get('RABBIT_RETRY_MIN_TIMEOUT')
-        .default(1000)
-        .asIntPositive(),
-      retries: env.get('RABBIT_RETRY_RETRIES').default(2).asIntPositive(),
+      minTimeout: env.get('RABBIT_RETRY_MIN_TIMEOUT').default(1000).asIntPositive(),
+      retries: env.get('RABBIT_RETRY_RETRIES').default(10).asIntPositive(),
       factor: env.get('RABBIT_RETRY_FACTOR').default(1.8).asFloatPositive(),
     },
   },
